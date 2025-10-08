6263e839d14444ebb8dbe7a57e97215dddafb314
Allen Nghayoui <70431642+allennghayoui@users.noreply.github.com>
2025-10-08 19:03:39 +0300

    Update sharphound.ps1

--- General/sharphound.ps1
+++ General/sharphound.ps1
@@ -3,7 +3,7 @@
 	Installs and runs the Sharphound data collector.
 
 	.DESCRIPTION
-	Installs the Sharphound data collector from a running Bloodhound instance and runs a scan querying all the domain info and uploads the results to an Amazon S3 Bucket.
+	Installs the Sharphound data collector from a running Bloodhound instance and runs a scan querying all the domain info and writes them to the console.
 
 	.EXAMPLE
 	PS> .\sharphound.ps1
@@ -81,7 +81,7 @@ function RemoveSharphoundZip
 		try
 		{
 			Write-Host "[*] Deleting '$sharphoundZipPath'..." -ForegroundColor Cyan
-			Remove-Item -Path $sharphoundZipPath -Force
+			Remove-Item -Path $sharphoundZipPath -Force -ErrorAction Stop
 			Write-Host "[*] Deleted '$sharphoundZipPath'." -ForegroundColor Cyan
 		} catch
 		{
@@ -98,7 +98,7 @@ function RemoveSharphoundFolder
 		try
 		{
 			Write-Host "[*] Deleting '$sharphoundPath'..." -ForegroundColor Cyan
-			Remove-Item -Path $sharphoundPath -Force -Recurse
+			Remove-Item -Path $sharphoundPath -Force -Recurse -ErrorAction Stop
 			Write-Host "[*] Deleted '$sharphoundPath'." -ForegroundColor Cyan
 		} catch
 		{
@@ -115,7 +115,7 @@ function RemoveSharphoundResultsFolder
 		try
 		{
 			Write-Host "[*] Deleting '$sharphoundResultsPath'..." -ForegroundColor Cyan
-			Remove-Item -Path $sharphoundResultsPath -Force -Recurse
+			Remove-Item -Path $sharphoundResultsPath -Force -Recurse -ErrorAction Stop
 			Write-Host "[*] Deleted '$sharphoundResultsPath'." -ForegroundColor Cyan
 		} catch
 		{
@@ -123,13 +123,6 @@ function RemoveSharphoundResultsFolder
 			exit 1
 		}
 	}
-}
-
-function CleanUp
-{
-	RemoveSharphoundZip
-	RemoveSharphoundFolder
-	RemoveSharphoundResultsFolder
 }
 
 function ShowProgress
@@ -142,11 +135,7 @@ function ShowProgress
 		[Parameter(Mandatory=$true)]
 		[int] $Id,
 		[Parameter(Mandatory=$false)]
-		[int] $ParentId,
-		[Parameter(Mandatory=$true)]
-		[ref] $CurrentTask,
-		[Parameter(Mandatory=$true)]
-		[string] $TotalTasks,
+		[Hashtable] $ProgressState,
 		[Parameter(Mandatory=$false)]
 		[switch] $Completed
 	)
@@ -156,26 +145,21 @@ function ShowProgress
 		Id = $Id
 	}
 
-	if ($null -ne $ParentId)
-	{
-		$argsList.ParentId = $ParentId
-	}
+	$percentage = ($ProgressState.CurrentTask / $ProgressState.TotalTasks) * 100
+	$progress = [math]::Round($percentage)
+	Write-Host "<PROGRESS>$progress%</PROGRESS>"
 
 	if ($Completed.IsPresent)
 	{
-		Write-Progress @argsList
+		Write-Progress @argsList -Completed
 	} else
 	{
-		$percentage = ($CurrentTask / $TotalTasks) * 100
-		$progress = [math]::Round($percentage)
-
-		Write-Host "<PROGRESS>$progress%</PROGRESS>"
-
 		$argsList.CurrentOperation = $CurrentOperation
 		$argsList.PercentComplete = $progress
 
 		Write-Progress @argsList
-		$CurrentTask = $CurrentTask + 1
+
+		$ProgressState.CurrentTask = $ProgressState.CurrentTask + 1
 	}
 }
 
@@ -184,7 +168,7 @@ function ShowProgress
 # Progress
 $ProgressState = @{
 	CurrentTask = 1
-	TotalTasks = 10
+	TotalTasks =  11
 }
 
 # Paths
@@ -192,26 +176,26 @@ $tempPath = "$env:TEMP"
 $sharphoundZipPath = "$tempPath\sharphound.zip"
 $sharphoundPath = "$tempPath\sharphound"
 $sharphoundResultsPath = "$tempPath\SharphoundResults"
+$sharphoundZipFiles = "$sharphoundResultsPath\ZipFiles"
+$sharphoundJsonFiles = "$sharphoundResultsPath\JSONFiles"
 $sharphoundExePath = "$sharphoundPath\Sharphound.exe"
 
-# Other
-$scanGuidPrefix = [guid]::NewGuid().ToString() 
-
-######################################## Script Start ########################################
-
-# Disable Windows Virtus and Threat Protection
-Write-Warning "[*] Downloading/Running Sharphound requires Windows Protection to be disabled temporarily..."
-
-ShowProgress -Id 0 -CurrentTask ([ref]$ProgressState.CurrentTask) -TotalTasks $ProgressState.TotalTasks -Activity "Sharphound Installation and Scan" -CurrentOperation "Disabling Windows Protection Temporarily..."
-
-# Original values
+# Windows Protection Original values
 $realtimeMonitoringOriginal = (Get-MpPreference).DisableRealtimeMonitoring
 $mapsReportingOriginal = (Get-MpPreference).MAPSReporting
 $submitSamplesConsentOriginal = (Get-MpPreference).SubmitSamplesConsent
 
+######################################## Script Start ########################################
+
+# Task 1: Disable Windows Virtus and Threat Protection
+ShowProgress -Id 0 -ProgressState $ProgressState -Activity "Sharphound Installation and Scan" -CurrentOperation "Disabling Windows Protection Temporarily..."
+
+Write-Warning "[*] Downloading/Running Sharphound requires Windows Protection to be disabled temporarily..."
+
 ManageWindowsProtection -Disable
 
-ShowProgress -Id 0 -CurrentTask ([ref]$ProgressState.CurrentTask) -TotalTasks $ProgressState.TotalTasks -Activity "Sharphound Installation and Scan" -CurrentOperation "Downloading the Sharphound Archive..."
+# Task 2: Download Sharphound Archive
+ShowProgress -Id 0 -ProgressState $ProgressState -Activity "Sharphound Installation and Scan" -CurrentOperation "Downloading the Sharphound Archive..."
 
 try
 {
@@ -230,14 +214,14 @@ try
 	exit 1
 }
 
-# Extracting downloaded Sharphound archive
+# Task 3: Extract Downloaded Sharphound Archive
+ShowProgress -Id 0 -ProgressState $ProgressState -Activity "Sharphound Installation and Scan" -CurrentOperation "Extracting Sharphound Archive..."
+
 try
 {
-	ShowProgress -Id 0 -CurrentTask ([ref]$ProgressState.CurrentTask) -TotalTasks $ProgressState.TotalTasks -Activity "Sharphound Installation and Scan" -CurrentOperation "Extracting Sharphound Archive..."
-
 	Write-Host "[*] Extracting $sharphoundZipPath..." -ForegroundColor Cyan
 
-	Expand-Archive -Path $sharphoundZipPath $sharphoundPath -ErrorAction Stop
+	Expand-Archive -Path $sharphoundZipPath $sharphoundPath -Force -ErrorAction Stop
 
 	Write-Host "[*] Extracted $sharphoundZipPath into $sharphoundPath." -ForegroundColor Cyan
 } catch
@@ -252,16 +236,22 @@ try
 	exit 1
 }
 
-# Create directory for Sharphound results
-ShowProgress -Id 0 -CurrentTask ([ref]$ProgressState.CurrentTask) -TotalTasks $ProgressState.TotalTasks -Activity "Sharphound Installation and Scan" -CurrentOperation "Creating Directory for Sharphound Results..."
+# Task 4: Create Directories for Sharphound Results
+ShowProgress -Id 0 -ProgressState $ProgressState -Activity "Sharphound Installation and Scan" -CurrentOperation "Creating Directory for Sharphound Results..."
 
 try
 {
-	Write-Host "[*] Creating Sharphound results directory $sharphoundResultsPath..." -ForegroundColor Cyan
-
+	Write-Host "[*] Creating Sharphound results directory '$sharphoundResultsPath'..." -ForegroundColor Cyan
 	New-Item -Path $sharphoundResultsPath -ItemType Directory -Force -ErrorAction Stop
-
-	Write-Host "[*] Created Sharphound results directory $sharphoundResultsPath." -ForegroundColor Cyan
+	Write-Host "[*] Created Sharphound results directory '$sharphoundResultsPath'." -ForegroundColor Cyan
+	
+	Write-Host "[*] Creating Sharphound zip files directory '$sharphoundZipFiles'..." -ForegroundColor Cyan
+	New-Item -Path $sharphoundZipFiles -ItemType Directory -Force -ErrorAction Stop
+	Write-Host "[*] Created Sharphound results directory '$sharphoundZipFiles'." -ForegroundColor Cyan
+	
+	Write-Host "[*] Creating Sharphound results directory '$sharphoundJsonFiles'..." -ForegroundColor Cyan
+	New-Item -Path $sharphoundJsonFiles -ItemType Directory -Force -ErrorAction Stop
+	Write-Host "[*] Created Sharphound results directory '$sharphoundJsonFiles'." -ForegroundColor Cyan
 } catch
 {
 	Write-Error "[!] Failed to create directory: '$sharphoundResultsPath'."
@@ -272,18 +262,18 @@ try
 	exit 1
 }
 
-# Run Sharphound scan
-ShowProgress -Id 0 -CurrentTask ([ref]$ProgressState.CurrentTask) -TotalTasks $ProgressState.TotalTasks -Activity "Sharphound Installation and Scan" -CurrentOperation "Running Sharphound Scan..."
+# Task 5: Run Sharphound Scan
+ShowProgress -Id 0 -ProgressState $ProgressState -Activity "Sharphound Installation and Scan" -CurrentOperation "Running Sharphound Scan..."
 
 try
 {
 	Write-Host "[*] Running Sharphound..." -ForegroundColor Cyan
 
-	Start-Process -Wait -FilePath $sharphoundExePath -ArgumentList "-c All --OutputPrefix $scanGuidPrefix --OutputDirectory $sharphoundResultsPath" -ErrorAction Stop
+	Start-Process -Wait -FilePath $sharphoundExePath -ArgumentList "-c All --OutputDirectory $sharphoundZipFiles" -ErrorAction Stop
 
-	$resultZipFiles = (Get-ChildItem -Path $sharphoundResultsPath -Filter "*_Bloodhound.zip").Name -join ", "
+	$zipResultFiles = Get-ChildItem -Path $sharphoundZipFiles -Filter "*_Bloodhound.zip"
 
-	Write-Host "[*] Sharphound scan is done. The results are contained in the following zip files: $resultZipFiles" -ForegroundColor Cyan
+	Write-Host "[*] Sharphound scan is done. The results are contained in the following zip files: $($zipResultFiles.Name -join ', ')" -ForegroundColor Cyan
 } catch
 {
 	Write-Error "[!] Error running Sharphound."
@@ -291,47 +281,114 @@ try
 	
 	RemoveSharphoundZip
 	RemoveSharphoundFolder
+	RemoveSharphoundResultsFolder
 	
 	ManageWindowsProtection -Reset
 	
 	exit 1
 }
 
-ShowProgress -Id 0 -CurrentTask ([ref]$ProgressState.CurrentTask) -TotalTasks $ProgressState.TotalTasks -Activity "Sharphound Installation and Scan" -CurrentOperation "Resetting Windows Protection to Previous State..."
+# Task 6: Reset Windows Protection to Previous State
+ShowProgress -Id 0 -ProgressState $ProgressState -Activity "Sharphound Installation and Scan" -CurrentOperation "Resetting Windows Protection to Previous State..."
 
 # Reset Windows Protection
 ManageWindowsProtection -Reset
 
-# Upload Sharphound scan results to S3 Bucket
-ShowProgress -Id 0 -CurrentTask ([ref]$ProgressState.CurrentTask) -TotalTasks $ProgressState.TotalTasks -Activity "Sharphound Installation and Scan" -CurrentOperation "Uploading Sharphound Results to Amazon S3..."
+# Task 7: Create New Directory to Store JSON Results
+ShowProgress -Id 0 -ProgressState $ProgressState -Activity "Sharphound Installation and Scan" -CurrentOperation "Creating new directory to store JSON results..."
 
-$currentFileUpload = 1
-$totalFileUploads = $resultZipFiles.Length
-$fileUploadProgress = $null
-foreach ($result in $resultZipFiles)
+try
 {
-	ShowProgress -Id 1 -ParentId 0 -CurrentTask $currentFileUpload -TotalTasks $totalFileUploads -Activity "Uploading Sharphound Result ZIP file: '$result'..."
+	Write-Host "[*] Creating new directory to store JSON results..." -ForegroundColor Cyan
+	New-Item -Path $sharphoundJsonFiles -ItemType Directory -Force -ErrorAction Stop
+	Write-Host "[*] Created new directory to store JSON results." -ForegroundColor Cyan
+} catch
+{
+	Write-Error "[!] Failed to create new directory to store JSON results."
+	Write-Error $_.Exception.Message
+		
+	RemoveSharphoundZip
+	RemoveSharphoundFolder
+	RemoveSharphoundResultsFolder
 
+	exit 1
+}
+
+# Task 8: Extract Result Files into New Directory
+ShowProgress -Id 0 -ProgressState $ProgressState -Activity "Sharphound Installation and Scan" -CurrentOperation "Extracting result files into new directory: '$sharphoundJsonFiles'..."
+
+Write-Host "[*] Extracting result files into new directory: '$sharphoundJsonFiles'..." -ForegroundColor Cyan
+
+foreach ($result in $zipResultFiles)
+{
 	try
 	{
-		Write-Host "[*] Uploading '$result' to S3 bucket..." -ForegroundColor Cyan
+		Write-Host "[*] Extracting '$result'..." -ForegroundColor Cyan
 
-		Write-S3Object -BucketName "tmp-bloodhound" -File "$sharphoundResultsPath\$result" -Key "$scanGuidPrefix/$result"
+		Expand-Archive -Path "$sharphoundZipFiles\$result" -DestinationPath "$sharphoundJsonFiles" -Force -ErrorAction Stop
 
-		Write-Host "[*] Uploaded '$result' to S3 bucket" -ForegroundColor Cyan
+		Write-Host "[*] Extracted '$result'." -ForegroundColor Cyan
 	} catch
 	{
-		Write-Error "[!] Failed to upload file: '$result'."
+		Write-Error "[!] Failed to extract '$result'."
 		Write-Error $_.Exception.Message
+		
+		RemoveSharphoundZip
+		RemoveSharphoundFolder
+		RemoveSharphoundResultsFolder
+		
 		exit 1
 	}
 }
-ShowProgress -Id 1 -ParentId 0 -CurrentTask $currentFileUpload -TotalTasks $totalFileUploads -Activity "Uploading Sharphound Result ZIP file: '$result'..." -Completed
+Write-Host "[*] Extracted result files into new directory: '$sharphoundJsonFiles'." -ForegroundColor Cyan
+
+# Task 9: Write JSON File Content to Console
+ShowProgress -Id 0 -ProgressState $ProgressState -Activity "Sharphound Installation and Scan" -CurrentOperation "Writing JSON file content to console..."
+
+Write-Host "[*] Writing content of JSON files to console..." -ForegroundColor Cyan
+
+$jsonResultFiles = Get-ChildItem -Path $sharphoundJsonFiles -Filter "*.json"
+$jsonResultFilesCount = $jsonResultFiles.Length
+
+Write-Host "<TOTALFILES>$jsonResultFilesCount</TOTALFILES>" -ForegroundColor Cyan
+
+for ($currentJsonFile = 1; $currentJsonFile -lt $jsonResultFilesCount; $currentJsonFile++)
+{
+	try
+	{
+		Write-Host "[*] Writting '$jsonFile' to console..." -ForegroundColor Cyan
+
+		$fileContent = Get-Content -Path "$sharphoundJsonFiles\$jsonFile"
+		Write-Host "<FILE $currentJsonFile/$jsonResultFilesCount>$fileContent</FILE $currentJsonFile/$jsonResultFilesCount>" -ForegroundColor Cyan
+
+		Write-Host "[*] Written '$jsonFile' to console." -ForegroundColor Cyan
+
+		Write-Host
+	} catch
+	{
+		Write-Error "[!] Failed to extract $result."
+		Write-Error $_.Exception.Message
+		
+		RemoveSharphoundZip
+		RemoveSharphoundFolder
+		RemoveSharphoundResultsFolder
+		
+		exit 1
+	}
+}
+Write-Host "[*] Written content of JSON files to console." -ForegroundColor Cyan
+
+# Task 10: Clean Up Extra Files
+ShowProgress -Id 0 -ProgressState $ProgressState -Activity "Sharphound Installation and Scan" -CurrentOperation "Cleaning up..."
 
 # Clean up
-CleanUp
+Write-Host "[*] Cleaning up..." -ForegroundColor Cyan
+RemoveSharphoundZip
+RemoveSharphoundFolder
+RemoveSharphoundResultsFolder
+Write-Host "[*] Clean up done." -ForegroundColor Cyan
 
-# Complete the progress bar
-ShowProgress -Id 0 -CurrentTask ([ref]$ProgressState.CurrentTask) -TotalTasks $ProgressState.TotalTasks -Activity "Sharphound Installation and Scan" -Completed
+# Task 11: Complete the progress bar
+ShowProgress -Id 0 -ProgressState $ProgressState -Activity "Sharphound Installation and Scan" -Completed
 
 exit 0
